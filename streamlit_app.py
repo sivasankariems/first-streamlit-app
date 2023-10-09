5f0e163095d93a64b14f48ddecf788800ebd3a86
sivasankariems <147391702+sivasankariems@users.noreply.github.com>
2023-10-09 16:19:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title("My Parents new healthy diner
+streamlit.title("My Parents new healthy diner")
+streamlit.header("Breakfast Menu")
+strealit.text("Egg")
 
-                Breakfast Menu")
 
