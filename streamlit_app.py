0dcd6b9e23077c09fa3c99e47be408ed6debe13c
sivasankariems <147391702+sivasankariems@users.noreply.github.com>
2023-10-15 15:57:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-fruits_selected=streamlit.multiselect("Pick some Fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some Fruits:",list(my_fruit_list.index),['Orange','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
                       
@@ -23,7 +23,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','pineapple')
 streamlit.write('The user entered ', fruit_choice)
 
 
@@ -39,7 +39,7 @@ my_data_row = my_cur.fetchall()
 streamlit.header("Fruits from the list Contains")
 streamlit.dataframe(my_data_row)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','Mango')
+fruit_choice = streamlit.text_input('What fruit would you like to add?','banana')
 streamlit.write('The user entered ', fruit_choice)
 
 
